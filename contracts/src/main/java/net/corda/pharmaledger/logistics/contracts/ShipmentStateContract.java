3d6697496ab1e61610053eb932c94d6bfb037da3
Anand Devarajan <anand.devarajan@tum.de>
2021-06-16 00:48:38 +0200

    shipment and kit state contracts

--- contracts/src/main/java/net/corda/pharmaledger/logistics/contracts/ShipmentStateContract.java
+++ contracts/src/main/java/net/corda/pharmaledger/logistics/contracts/ShipmentStateContract.java
@@ -1,13 +1,27 @@
 package net.corda.pharmaledger.logistics.contracts;
 
-import net.corda.core.contracts.Contract;
-import net.corda.core.transactions.LedgerTransaction;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
+import net.corda.core.contracts.Contract;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.transactions.LedgerTransaction;
+import net.corda.pharmaledger.logistics.states.ShipmentState;
+
+import static net.corda.core.contracts.ContractsDSL.requireThat;
 public class ShipmentStateContract implements Contract {
 
     @Override
-    public void verify(LedgerTransaction arg0) throws IllegalArgumentException {
-        // TODO Auto-generated method stub
+    public void verify(LedgerTransaction tx) throws IllegalArgumentException {
+        List<ContractState> outputs = tx.getOutputStates();
+		requireThat(require -> {
+			ShipmentState shipment = (ShipmentState) outputs.get(0);
+			require.using("ID should be greater than 0", shipment.getShipmentID() > 0);
+			require.using("Shipment code should not be empty", !StringUtils.isEmpty(shipment.getShipmentLabel().get("code").getAsString()));
+			require.using("shipment status should not be empty", !StringUtils.isEmpty(shipment.getStatus()));
+            return null;
+		});
         
     }
 
--- contracts/src/main/java/net/corda/pharmaledger/pharma/contracts/KitStateContract.java
+++ contracts/src/main/java/net/corda/pharmaledger/pharma/contracts/KitStateContract.java
@@ -1,14 +1,28 @@
 package net.corda.pharmaledger.pharma.contracts;
 
-import net.corda.core.contracts.Contract;
-import net.corda.core.transactions.LedgerTransaction;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
+import net.corda.core.contracts.Contract;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.transactions.LedgerTransaction;
+import net.corda.pharmaledger.pharma.states.KitState;
+
+import static net.corda.core.contracts.ContractsDSL.requireThat;
 public class KitStateContract implements Contract {
 
     @Override
-    public void verify(LedgerTransaction arg0) throws IllegalArgumentException {
+    public void verify(LedgerTransaction tx) throws IllegalArgumentException {
         // TODO Auto-generated method stub
-        
+        List<ContractState> outputs = tx.getOutputStates();
+        requireThat(require -> {
+            KitState kit = (KitState) outputs.get(0);
+			require.using("ID should be greater than 0", kit.getKitID() > 0);
+            // Patient and Shipment contracts verified...
+            require.using("Medication should not be empty",!kit.getMedication().isEmpty() );
+            return null;
+        });
     }
 
 }    
