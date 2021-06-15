2763d3c77f00abff6980b2b77ab5b6de4395ea9c
Thandayuthapani Subramanian <ge79wom@mytum.de>
2021-06-15 23:56:08 +0200

    Add contract for Patient State

---
+++ contracts/src/main/java/net/corda/pharmaledger/Constants.java
@@ -0,0 +1,8 @@
+package net.corda.pharmaledger;
+
+public class Constants {
+    public static final String BIOMETRIC_WEIGHT = "weight";
+    public static final String BIOMETRIC_AGE = "age";
+    public static final String BIOMETRIC_HEIGHT = "height";
+    public static final String BIOMETRIC_GENDER = "gender";
+}
--- contracts/src/main/java/net/corda/pharmaledger/medical/contracts/PatientStateContract.java
+++ contracts/src/main/java/net/corda/pharmaledger/medical/contracts/PatientStateContract.java
@@ -1,14 +1,33 @@
 package net.corda.pharmaledger.medical.contracts;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
 import net.corda.core.contracts.Contract;
+import net.corda.core.contracts.ContractState;
 import net.corda.core.transactions.LedgerTransaction;
+import net.corda.pharmaledger.Constants;
+import net.corda.pharmaledger.medical.states.PatientState;
+
+import static net.corda.core.contracts.ContractsDSL.requireThat;
 
 public class PatientStateContract implements Contract {
 
 	@Override
-	public void verify(LedgerTransaction arg0) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		
+	public void verify(LedgerTransaction tx) throws IllegalArgumentException {
+		List<ContractState> outputs = tx.getOutputStates();
+		requireThat(require -> {
+			PatientState patient = (PatientState) outputs.get(0);
+			require.using("ID should be greater than 0", patient.getPatientID() > 0);
+			require.using("Name should not be empty", !StringUtils.isEmpty(patient.getMediStaff()));
+			require.using("Address should not be empty", !StringUtils.isEmpty(patient.getPatientAddress()));
+			require.using("Weight should not be greater than 200KGs", patient.getPatientBiometricData().get(Constants.BIOMETRIC_WEIGHT).getAsInt() > 200);
+			require.using("Age should not be greater than 150", patient.getPatientBiometricData().get(Constants.BIOMETRIC_AGE).getAsInt() > 150);
+			require.using("Height should not be greater than 300cm", patient.getPatientBiometricData().get(Constants.BIOMETRIC_HEIGHT).getAsInt() > 300);
+			require.using("Gender should not be empty", !StringUtils.isEmpty(patient.getPatientBiometricData().get(Constants.BIOMETRIC_GENDER).getAsString()));
+			return null;
+		});
 	}
     
 }
