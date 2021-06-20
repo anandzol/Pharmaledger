59559c9d42443639023cb2fbabca93b3b1d0d4ad
Anand Devarajan <anand.devarajan@tum.de>
2021-06-20 16:39:25 +0200

    send patientevaluation flow - initial cut

---
+++ contracts/src/main/java/net/corda/pharmaledger/medical/contracts/PatientEvaluationStateContract.java
@@ -0,0 +1,33 @@
+package net.corda.pharmaledger.medical.contracts;
+
+import static net.corda.core.contracts.ContractsDSL.requireThat;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.Contract;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.transactions.LedgerTransaction;
+import net.corda.pharmaledger.medical.states.PatientEvaluationState;
+
+public class PatientEvaluationStateContract implements Contract{
+
+    @Override
+    public void verify(LedgerTransaction tx) throws IllegalArgumentException {
+        List<ContractState> outputs = tx.getOutputStates();
+		requireThat(require -> {
+			PatientEvaluationState evaluation = (PatientEvaluationState) outputs.get(0);			
+			require.using("Evaluation Result should not be empty", !StringUtils.isEmpty(evaluation.getEvaluationResult()) );
+            return null;
+		});
+        
+    }
+
+    
+	public interface Commands extends CommandData {
+        class Create implements Commands {}
+    }
+    
+}
---
+++ contracts/src/main/java/net/corda/pharmaledger/medical/states/PatientEvaluationState.java
@@ -0,0 +1,96 @@
+package net.corda.pharmaledger.medical.states;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import net.corda.core.contracts.BelongsToContract;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.AnonymousParty;
+import net.corda.pharmaledger.medical.contracts.PatientEvaluationStateContract;
+
+@BelongsToContract(PatientEvaluationStateContract.class)
+public class PatientEvaluationState implements ContractState {
+    private int patientID;
+    private String symptoms;
+    private Date evaluationDate;
+    private String  evaluationResult;
+    private AnonymousParty fromMedical;
+    private AnonymousParty toPharma;
+    private List<AbstractParty> participants;
+
+    public PatientEvaluationState(int patientID, String symptoms, Date evaluationDate, String evaluationResult, AnonymousParty fromMedical, AnonymousParty toPharma) {
+        this.patientID = patientID;
+        this.symptoms = symptoms;
+        this.evaluationDate = evaluationDate;
+        this.evaluationResult = evaluationResult;
+        this.fromMedical = fromMedical;
+        this.toPharma = toPharma;
+        this.participants = new ArrayList<AbstractParty>();
+        participants.add(fromMedical);
+        participants.add(toPharma);
+    }
+
+    public int getPatientID() {
+        return this.patientID;
+    }
+
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
+
+    public String getSymptoms() {
+        return this.symptoms;
+    }
+
+    public void setSymptoms(String symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public Date getEvaluationDate() {
+        return this.evaluationDate;
+    }
+
+    public void setEvaluationDate(Date evaluationDate) {
+        this.evaluationDate = evaluationDate;
+    }
+
+    public String getEvaluationResult() {
+        return this.evaluationResult;
+    }
+
+    public void setEvaluationResult(String evaluationResult) {
+        this.evaluationResult = evaluationResult;
+    }
+
+    public AnonymousParty getFromMedical() {
+        return this.fromMedical;
+    }
+
+    public void setFromMedical(AnonymousParty fromMedical) {
+        this.fromMedical = fromMedical;
+    }
+
+    public AnonymousParty getToPharma() {
+        return this.toPharma;
+    }
+
+    public void setToPharma(AnonymousParty toPharma) {
+        this.toPharma = toPharma;
+    }
+    public void setParticipants(List<AbstractParty> participants) {
+        this.participants = participants;
+    }
+
+
+
+
+    @Override
+    public List<AbstractParty> getParticipants() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+
+}
