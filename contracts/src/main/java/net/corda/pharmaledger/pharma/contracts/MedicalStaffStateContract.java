2139ffa13356ebc641b7fda07266a29548669863
Thandayuthapani Subramanian <ge79wom@mytum.de>
2021-06-29 22:46:31 +0200

    Add not empty check for Medical Staff contract

--- contracts/src/main/java/net/corda/pharmaledger/pharma/contracts/MedicalStaffStateContract.java
+++ contracts/src/main/java/net/corda/pharmaledger/pharma/contracts/MedicalStaffStateContract.java
@@ -18,13 +18,15 @@ public class MedicalStaffStateContract implements Contract {
 	@Override
 	public void verify(LedgerTransaction tx) throws IllegalArgumentException {
 		List<ContractState> outputs = tx.getOutputStates();
-		requireThat(require -> {
-			MedicalStaffState staff = (MedicalStaffState) outputs.get(0);
-			require.using("ID should be greater than 0", !staff.getStaffID().isEmpty());
-            require.using("Name should not be empty", !StringUtils.isEmpty(staff.getStaffName()));
-			require.using("Proficiency should not be empty", !StringUtils.isEmpty(staff.getProficiency()));
-			return null;
-		});
+		if (!outputs.isEmpty()) {
+			requireThat(require -> {
+				MedicalStaffState staff = (MedicalStaffState) outputs.get(0);
+				require.using("ID should be greater than 0", !staff.getStaffID().isEmpty());
+				require.using("Name should not be empty", !StringUtils.isEmpty(staff.getStaffName()));
+				require.using("Proficiency should not be empty", !StringUtils.isEmpty(staff.getProficiency()));
+				return null;
+			});
+		}
 	}
     
 }
